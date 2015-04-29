5b100b41905e9b667f22e9aa3e1fa2629a45f5fc
Christian Muise <christian.muise@gmail.com>
2015-04-30 09:38:12 +1000

    Adding the api.py files currently being generated.

---
+++ classical/grid/api.py
@@ -0,0 +1,10 @@
+domains = [
+{'description': '',
+ 'ipc': None,
+ 'name': 'grid',
+ 'problems': [('grid/domain.pddl', 'grid/prob01.pddl'),
+              ('grid/domain.pddl', 'grid/prob02.pddl'),
+              ('grid/domain.pddl', 'grid/prob03.pddl'),
+              ('grid/domain.pddl', 'grid/prob04.pddl'),
+              ('grid/domain.pddl', 'grid/prob05.pddl')]}
+]
---
+++ classical/maintenance-opt14-adl/api.py
@@ -0,0 +1,15 @@
+domains = [
+{'description': '',
+ 'ipc': '2014',
+ 'name': 'maintenance',
+ 'problems': [('maintenance-opt14-adl/domain.pddl',
+               'maintenance-opt14-adl/maintenance.1.3.010.010.2-000.pddl'),
+              ('maintenance-opt14-adl/domain.pddl',
+               'maintenance-opt14-adl/maintenance.1.3.010.010.2-001.pddl'),
+              ('maintenance-opt14-adl/domain.pddl',
+               'maintenance-opt14-adl/maintenance.1.3.010.010.2-002.pddl'),
+              ('maintenance-opt14-adl/domain.pddl',
+               'maintenance-opt14-adl/maintenance.1.3.015.020.2-001.pddl'),
+              ('maintenance-opt14-adl/domain.pddl',
+               'maintenance-opt14-adl/maintenance.1.3.015.020.2-002.pddl')]}
+]
