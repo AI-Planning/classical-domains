61e5fced2f8a04e9c07f55328707901c03290cf3
Christian Muise <christian.muise@ibm.com>
2018-06-29 11:46:24 +0200

    Adding the ipc-2018 domains.

---
+++ classical/flashfill-opt18/api.py
@@ -0,0 +1,4 @@
+domains = [{'description': '(opt18) Flashfill:\nsubmitted by: Javier Segovia, Universitat Pompeu Fabra\n\nFlashfill models the flashfill feature included in Excel that synthesize programs based on\nuser actions. The inputs are strings that must be transformed into other strings\n(e.g. extract the capital letters of the name and surname) and the output is a computer\nprogram that works on all instances. This can be encoded as a classical planning problem\nusing the planning programs compilation by Segovia et al.\n\nPublications: Javier Segovia Aguas, Sergio Jimenez Celorrio, Anders Jonsson: Generalized\nPlanning with Procedural Domain Control Knowledge. ICAPS 2016',
+ 'ipc': '2018',
+ 'name': 'flashfill',
+ 'problems': []}]
